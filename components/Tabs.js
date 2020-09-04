fceb8546131023add7efd909876611439ffe74fb
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-09-04 08:35:04 -0700

    Tabs completed pending QA. Moving on to Cards.

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,22 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+let topicContainer = document.querySelector('.topics');
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(stuff => {
+        let tempArray = Array.from(stuff.data.topics);
+
+        tempArray.forEach(topic => {
+            let newTopic = document.createElement('tab') //create new element. note in the HTML the top level element is tabs (plural) while this is singular
+            newTopic.classList.add('tab') //add class name to new element
+            newTopic.textContent = topic; //add text content from GET to each new element
+            topicContainer.appendChild(newTopic)    //append each new element to .topics
+
+        })
+
+    })
+    .catch(err => {
+        debugger
+    })
